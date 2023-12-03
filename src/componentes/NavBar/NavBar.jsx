946159d03cd97ad1b60c724e6cdb0cdbdf540e04
framsito <137754072+framsito@users.noreply.github.com>
2023-12-03 00:30:00 -0300

    Revert "Update NavBar.jsx" nvm This reverts commit 84285fd8280d4c79ac6a0db101981106fe219fd8.

--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <Link to="/" className="logo">
-        <img src={process.env.PUBLIC_URL + "/media/img/logo.png"} width={80} height={80} alt="Logo"></img>
+        <img src="../../media/img/logo.png" width={80} height={80}></img>
       </Link>
       <Link className="titulonav" to="/">
         AFA Shop
